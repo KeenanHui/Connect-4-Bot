de543d8c957abf0504acf472a3d089c7c6a899fd
keenanhui@utexas.edu <keenanhui@utexas.edu>
2026-02-05 00:26:11 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
 from anvil.js import get_dom_node  # ✅ supported way to style DOM
 
 class Form1(Form1Template):
